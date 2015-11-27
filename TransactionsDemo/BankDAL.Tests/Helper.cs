2bbcf0fbf5bf6bc7e028e60c7a63031630f72881
Samuel <scholtes.s@hotmail.com>
2015-11-27 22:18:50 +0100

    Ajout de la solution de départ du 28-11-2015

---
+++ TransactionsDemo/BankDAL.Tests/Helper.cs
@@ -0,0 +1,31 @@
+﻿using com.rusanu.DBUtil;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDAL.Tests
+{
+    public class Helper
+    {
+        public static void CreerBaseDeDonnees()
+        {
+            
+            using (SqlConnection conn = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLOCALDB;Integrated Security=True;"))
+            {
+                conn.Open();
+                var tempPath=Path.GetTempFileName();
+                File.WriteAllText(tempPath,Properties.Resources.CreateDatabase);
+                //SqlCmd est une classe définie dans la librairie DbUtil, dont le code a été récupéré
+                //du repository GitHub https://github.com/rusanu/DbUtilSqlCmd
+                SqlCmd.ExecuteFile(conn,tempPath);
+                conn.Close();
+            }
+        }
+    }
+}
