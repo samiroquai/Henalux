f1fb032f7606d01d0467367a16870d46a6b1582a
Samuel <scholtes.s@hotmail.com>
2015-11-27 22:59:15 +0100

    Ajout Modèle ChoixArchitecture

---
+++ ChoixArchitecture/Model/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ ChoixArchitecture/Model/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class SchoolContext : DbContext
+    {
+        static SchoolContext()
+        {
+            Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>());
+        }
+
+        public SchoolContext()
+            : base("Data Source=(LocalDB)\\MSSQLLOCALDB; Initial Catalog=ChoixArchitecture; Integrated Security=True;")
+        {
+
+        }
+    }
+}
