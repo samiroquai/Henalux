4895632e57a8ce94080b5bc85d0f56793c25e928
Samuel <samuel.scholtes@outlook.com>
2016-11-15 15:31:20 +0100

    Ajout démo ShellApp

---
+++ UWPExamples/ShellAppDemo/ShellAppDemo/Shell.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ShellAppDemo
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+
+        internal void ShowDefaultPage()
+        {
+            MainFrame.Navigate(typeof(MainPage));
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        internal bool DoesShowSomething()
+        {
+            return MainFrame.Content != null;
+        }
+
+        private void MenuButton2_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(OtherPage));
+        }
+
+        private void MenuButton1_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(MainPage));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
+        }
+    }
+}
